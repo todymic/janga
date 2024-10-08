b88698faa4f93ec9a04f077caacb8036c6a0b336
todymic <todisoa.razafindrabekoto@idp.lu>
2024-10-08 11:48:55 +0200

    reorganize folder

--- admin/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./templates/home/home.module').then(m => m.HomeModule)
-  }
-];
