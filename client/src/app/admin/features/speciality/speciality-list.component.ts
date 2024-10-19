53298e61b2a0da78820052dc72d0fbd828652538
todymic <todisoa.razafindrabekoto@idp.lu>
2024-10-19 16:32:09 +0200

    Speciality component

--- client/src/app/admin/features/speciality/speciality-list.component.ts
+++ client/src/app/admin/features/speciality/speciality-list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import {ListComponent} from "../page/list/list.component";
 
 @Component({
   selector: 'app-speciality-list',
   standalone: true,
   imports: [],
-  templateUrl: './speciality-list.component.html',
+  templateUrl: '../page/list/list.component.html',
   styleUrl: './speciality-list.component.scss'
 })
-export class SpecialityListComponent {
+export class SpecialityListComponent extends ListComponent {
+  override displayedColumns: string[] = ['id', 'name', 'action'];
+
 
 }
